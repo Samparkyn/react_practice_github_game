9c0643d8ac21aed99657f8aca8317d83ea0355bd
Sam Parkyn <samparkyn101@gmail.com>
2016-05-23 16:27:32 +0200

    added ConfirmBattle container and component. component is stateless functional component

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var ConfirmBattleContainer = React.createClass({
+  render: function(){
+    return (
+      <div>
+
+      </div>
+      )
+  }
+});
+
+module.exports = ConfirmBattleContainer;
