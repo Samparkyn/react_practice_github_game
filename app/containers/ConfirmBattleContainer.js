6780fded8003fb0c287e4352917c1a29b18ae2a2
Sam Parkyn <samparkyn101@gmail.com>
2016-05-23 16:36:02 +0200

    added confirm battle container to routes, and set the getInitialState

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,10 +1,22 @@
 var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
 
 var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.propTypes.object.isRequired
+  },
+
+  getInitialState: function(){
+    return {
+      isLoading: true,
+      playerInfo: []
+    }
+  },
+
   render: function(){
     return (
       <div>
-
+        <ConfirmBattle />
       </div>
       )
   }
