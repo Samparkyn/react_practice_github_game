08455daecfb3807cf8d3731e3c2ddd2e2e7390b8
Sam Parkyn <samparkyn101@gmail.com>
2016-05-23 17:34:15 +0200

    added helpers function & installed axios

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -3,20 +3,39 @@ var ConfirmBattle = require('../components/ConfirmBattle');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
-    router: React.propTypes.object.isRequired
+    router: React.PropTypes.object.isRequired
   },
 
   getInitialState: function(){
     return {
       isLoading: true,
-      playerInfo: []
+      playerInfo: [],
     }
+  },
+
+  componentWillMount: function(){ // following 4 functions are life cycle events
+
+  },
+
+  componentDidMount: function(){
+    var query = this.props.location.query;
+    //fetch info from github and then update state
+  },
+
+  componentWillReceiveProps: function(){
+
+  },
+
+  componentWillUnmount: function(){
+
   },
 
   render: function(){
     return (
       <div>
-        <ConfirmBattle />
+        <ConfirmBattle
+          isLoading={this.state.isLoading}
+          playersInfo={this.state.playersInfo} />
       </div>
       )
   }
