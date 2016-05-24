c889e65754b588ea4c82f552912ad66fec795657
Sam Parkyn <samparkyn101@gmail.com>
2016-05-24 10:17:22 +0200

    added PropTypes to Confirm battle & route for results page

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,16 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
 function ConfirmBattle(props){
   return props.isLoading === true
   ? <p> Loading! </p>
   : <p> Confirm Battle </p>
-};
+}
+
+ConfirmBattle.propTypes = {
+isLoading: PropTypes.bool.isRequired,
+onInitiateBattle: PropTypes.func.isRequired,
+playersInfo: PropTypes.array.isRequired
+}
 
 module.exports = ConfirmBattle;
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,6 +1,21 @@
+var axios = require('axios');
+
+function getUserInfo(username){
+  return axios.get('https://api.github.com/users/' + username)
+}
+
 var helpers = {
-  getPlayersInfo: function(){
+  getPlayersInfo: function(players){
     //fetch some data from github
+    return axios.all(players.map(function(username){
+      return getUserInfo(username)
+    })).then(function(info){
+      return info.map(function(user){
+        return user.data;
+      })
+    }).catch(function(err){
+      console.warn('Error in getPlayersInfo', err)
+    })
   }
 };
 
